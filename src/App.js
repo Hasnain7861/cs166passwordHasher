7905de8780e76395cb2951743a2cebe2a8c51ccc
Hasnain Mucklai <mucklaihasnain@gmail.com>
2023-11-28 21:42:21 -0800

    Password Hasher working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import bcrypt from 'bcryptjs';
+import './App.css'; 
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [hashedPassword, setHashedPassword] = useState('');
+
+  const handleInputChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+      setHashedPassword(hash);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="password-form" onSubmit={handleFormSubmit}>
+        <label>
+          Enter Password:
+          <input type="password" value={password} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Hash Password</button>
+      </form>
+
+      {hashedPassword && (
+        <div className="result-container">
+          <h3>Hashed Password:</h3>
+          <p>{hashedPassword}</p>
+        </div>
+      )}
     </div>
   );
 }
